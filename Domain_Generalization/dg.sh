faa152a649716bdf7249e6ba767e266f77200220
yuqi_lab <42293725+wyqjy@users.noreply.github.com>
2022-07-07 17:55:49 +0800

    Initial commit

---
+++ Domain_Generalization/dg.sh
@@ -0,0 +1,38 @@
+
+
+D1=art_painting
+D2=cartoon
+D3=photo
+D4=sketch
+
+for SEED in $(seq 0 0)
+do
+    for SETUP in $(seq 1 4)
+    do
+        if [ ${SETUP} == 1 ]; then
+            S1=${D2}
+            S2=${D3}
+            S3=${D4}
+            T=${D1}
+        elif [ ${SETUP} == 2 ]; then
+            S1=${D1}
+            S2=${D3}
+            S3=${D4}
+            T=${D2}
+        elif [ ${SETUP} == 3 ]; then
+            S1=${D1}
+            S2=${D2}
+            S3=${D4}
+            T=${D3}
+        elif [ ${SETUP} == 4 ]; then
+            S1=${D1}
+            S2=${D2}
+            S3=${D3}
+            T=${D4}
+        fi
+
+        python train.py \
+        --source ${S1} ${S2} ${S3} \
+        --target ${T}
+    done
+done
