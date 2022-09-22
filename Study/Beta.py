7a86bdc9492bdf0e55a64b617c27d2458c548b7f
yuqi_lab <42293725+wyqjy@users.noreply.github.com>
2022-09-22 15:48:26 +0800

    修改Beta计算

--- Study/Beta.py
+++ Study/Beta.py
@@ -17,27 +17,32 @@ def vis(x, y):
     plt.plot(x, y)
     plt.show()
 
+
 x = []
 y = []
 max_beta = 0.6
 mixup_step = 10
-
-x_pow = []
-y_pow = []
 for epoch in range(30):
-
     ''' 原来的线性'''
     x.append(epoch)
-    # mixup_beta = min(max_beta, max(max_beta * (epoch) / mixup_step, 0.1))
-    # y.append(mixup_beta)
+    mixup_beta = min(max_beta, max(max_beta * (epoch) / mixup_step, 0.1))
+    y.append(mixup_beta)
     # print(epoch, ' : ', mixup_beta)
 
-    a=1.1  # 从1.1-1.5中取值
-    ''' 指数函数 左侧部分<1  '''
-    pow_beta = min(max_beta, math.pow(a, epoch-30))  # 加不加最小值0.1待定
-    y_pow.append(pow_beta)
-    print(epoch, ':', pow_beta, ' '*10, math.pow(a, epoch-30))
 
+def fun(a):
+    x_pow = []
+    y_pow = []
+    for epoch in range(30):
+        ''' 指数函数 左侧部分<1  '''
+        pow_beta = min(max_beta, math.pow(a, epoch-30))  # 加不加最小值0.1待定
+        y_pow.append(pow_beta)
+        print(epoch, ':', pow_beta, ' '*10, math.pow(a, epoch-30))
+    return y_pow
 
-# vis(x, y)
-vis(x, y_pow)
+vis(x, y)
+a=1.1  # 从1.1-1.5中取值
+for i in range(5):
+    y_pow = fun(a)
+    a+=0.1
+    vis(x, y_pow)
