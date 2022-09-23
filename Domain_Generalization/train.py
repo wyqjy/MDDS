f1b25fb9dc51d641e633497400f38d0015b9fd40
yuqi_lab <42293725+wyqjy@users.noreply.github.com>
2022-09-23 17:57:28 +0800

    [15-1] officehome

--- Domain_Generalization/train.py
+++ Domain_Generalization/train.py
@@ -34,7 +34,7 @@ def get_args():
     parser.add_argument("--source", choices=available_datasets, help="Source", nargs='+')
     parser.add_argument("--target", choices=available_datasets, help="Target")
     parser.add_argument("--batch_size", "-b", type=int, default=64, help="Batch size")  #受内存限制 改为32
-    parser.add_argument("--image_size", type=int, default=224, help="Image size")  # digits32(Lenet)  vlcs (alex) 224  resnet222
+    parser.add_argument("--image_size", type=int, default=222, help="Image size")  # digits32(Lenet)  vlcs (alex) 224  resnet222
     # data aug stuff
     parser.add_argument("--min_scale", default=0.8, type=float, help="Minimum scale percent")
     parser.add_argument("--max_scale", default=1.0, type=float, help="Maximum scale percent")
@@ -46,7 +46,7 @@ def get_args():
                         help="If set, it will limit the number of training samples")
     parser.add_argument("--limit_target", default=None, type=int,
                         help="If set, it will limit the number of testing samples")
-    parser.add_argument("--learning_rate", "-l", type=float, default=.001, help="Learning rate")
+    parser.add_argument("--learning_rate", "-l", type=float, default=.01, help="Learning rate")
     parser.add_argument("--epochs", "-e", type=int, default=30, help="Number of epochs")  #默认20
     parser.add_argument("--n_classes", "-c", type=int, default=7, help="Number of classes")
     parser.add_argument("--network", choices=model_factory.nets_map.keys(), help="Which network to use", default="resnet50")
@@ -193,27 +193,27 @@ class Trainer:
 
         return class_correct
 
-    def lr_Adjust(self, epoch):
-
-        if epoch==10:
-            for params in self.optimizer.param_groups:
-                params['lr'] = 0.01
-        # if epoch > 20 and epoch < 35:
-        #     for params in self.optimizer.param_groups:
-        #         params['lr'] *= 0.999
-        if epoch==30:
-            for params in self.optimizer.param_groups:
-                params['lr'] = 0.001
-        if epoch>30:
-            for params in self.optimizer.param_groups:
-                params['lr'] *= 0.99
-                # if epoch==30:
-                #     params['lr'] = 0.001
-                # elif epoch<= 70:
-                #     params['lr'] *= 0.83
-                # else:
-                #     params['lr'] *= self.dec_lr
-                #     self.dec_lr -= 0.015
+    # def lr_Adjust(self, epoch):
+    #
+    #     if epoch==10:
+    #         for params in self.optimizer.param_groups:
+    #             params['lr'] = 0.01
+    #     # if epoch > 20 and epoch < 35:
+    #     #     for params in self.optimizer.param_groups:
+    #     #         params['lr'] *= 0.999
+    #     if epoch==30:
+    #         for params in self.optimizer.param_groups:
+    #             params['lr'] = 0.001
+    #     if epoch>30:
+    #         for params in self.optimizer.param_groups:
+    #             params['lr'] *= 0.99
+    #             # if epoch==30:
+    #             #     params['lr'] = 0.001
+    #             # elif epoch<= 70:
+    #             #     params['lr'] *= 0.83
+    #             # else:
+    #             #     params['lr'] *= self.dec_lr
+    #             #     self.dec_lr -= 0.015
 
     def only_test(self):
         '''  用训练好的模型测试，不训练  '''
