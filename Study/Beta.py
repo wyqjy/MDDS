afd5db56c7df98a24aca84d98e61bf8500238af2
yuqi_lab <42293725+wyqjy@users.noreply.github.com>
2022-10-06 12:39:01 +0800

    beta画图

--- Study/Beta.py
+++ Study/Beta.py
@@ -40,7 +40,7 @@ def fun(a):
         ''' 指数函数 左侧部分<1  '''
         pow_beta = min(max_beta, math.pow(a, epoch-20))  # 加不加最小值0.1待定
         y_pow.append(pow_beta)
-        print(epoch, ':', pow_beta, ' '*10, math.pow(a, epoch-20))
+        # print(epoch, ':', pow_beta, ' '*10, math.pow(a, epoch-20))
     return y_pow
 
 def lamuda():
@@ -48,12 +48,57 @@ def lamuda():
     print(RG.beta(max_beta, max_beta, size=10))
     print(torch.from_numpy(RG.beta(max_beta, max_beta, size=10)).float())
 
-#
+# orgin()
 # vis(x, y)
+# fig, ax = plt.subplots()
 # a=1.1  # 从1.1-1.5中取值
-# for i in range(1):
+# for i in range(5):
 #     y_pow = fun(a)
-#     a+=0.1
-#     vis(x, y_pow)
+#     ax.plot(x, y_pow, label='a='+str(a))
+#     ax.legend()
+#     a += 0.1
+#     a = round(a, 1)
+#     print(a)
+#     # vis(x, y_pow)
+# ax.set_xlabel('epoch')
+# ax.set_ylabel('alpha')
+# # plt.savefig('Beta 参数变化 a.jpg')
+# plt.show()
+# # lamuda()
 
-lamuda()
+
+def Be():
+    import numpy as np
+    from scipy.stats import beta
+    import matplotlib.pyplot as plot
+
+    # 设置 plot 支持中文
+    from matplotlib.font_manager import FontProperties
+    font = FontProperties(fname=r"c:\windows\fonts\SimSun.ttc", size=14)
+
+    # 定义一组alpha 跟 beta值
+    # alpha_beta_values = [[0.5, 0.5], [5, 1], [1, 3], [2, 2], [2, 5]]
+    alpha_beta_values = [[0.1, 0.1], [0.2, 0.2], [0.3, 0.3], [0.5, 0.5]]
+    linestyles = []
+
+    # 定义 x 值
+    x = np.linspace(0, 1, 1002)[1:-1]
+    for alpha_beta_value in alpha_beta_values:
+        print(alpha_beta_value)
+        dist = beta(alpha_beta_value[0], alpha_beta_value[1])
+        dist_y = dist.pdf(x)
+        # 添加图例
+        # plot.legend('alpha=')
+        # 创建 beta 曲线
+        plot.plot(x, dist_y, label=r'$\alpha=%.1f,\ \beta=%.1f$' % (alpha_beta_value[0], alpha_beta_value[1]))
+
+    # 设置标题
+    plot.title(u'Beta分布', fontproperties=font)
+    # 设置 x,y 轴取值范围
+    plot.xlim(0, 1)
+    plot.ylim(0, 2.5)
+    plot.legend()
+    plt.savefig('Beta分布.jpg')
+    plot.show()
+
+Be()
